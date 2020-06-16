1e1e9dc735ea113bf90f93df4fef6170755cbc8f
SergioGitHub8 <60387304+SergioGitHub8@users.noreply.github.com>
2020-06-16 22:47:50 +0200

    Update App
    Finalizand App

--- FlixNet/src/main/java/edu/es/eoi/App.java
+++ FlixNet/src/main/java/edu/es/eoi/App.java
@@ -10,7 +10,7 @@ public class App {
 	
 	public static Playable[] playList= new Playable[10];
 	public static PlayService service= new PlayService();
-	public static PlayController controller= new PlayController(service);	
+	public static PlayController controller= new PlayController();	
 
 	public static void main(String[] args) {
 	
@@ -20,18 +20,18 @@ public class App {
 	}
 	
 	public static void createPlayOffer() {		
-		Film film1= new Film();
-		film1.setName("La Jungla de cristal 1");
-		Film film2= new Film();
-		film2.setName("La Jungla de cristal 2");
-		Film film3= new Film();
-		film3.setName("La Jungla de cristal 3");
-		Film film4= new Film();
-		film4.setName("La Jungla de cristal 4");
+		Film film1 = new Film();
+		film1.setName("El hobbit: un viaje inesperado");
+		
+		Film film2 = new Film();
+		film2.setName("El hobbit: la desolación de Smaug");
+		
+		Film film3 = new Film();
+		film3.setName("El Hobbit: la batalla de los cinco ejércitos");
+		
 		playList[0]=film1;
 		playList[1]=film2;
 		playList[2]=film3;
-		playList[3]=film4;		
 		
 	}
 	
